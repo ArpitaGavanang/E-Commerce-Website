36b34dbe4fe580fd44aac992aec7de5b47e8b872
Your Name <you@example.com>
2024-09-26 20:23:02 +0530

    First Commit

---
+++ backend/data.js
@@ -0,0 +1,60 @@
+export default {
+    products: [
+        {
+            _id: "1",
+            name: "Nike Men's Regular fit T-Shirt",
+            category: "Shirt",
+            image: "/images/d1.jpg",
+            price: 35,
+            brand: " Nike",
+            rating: 3.0,
+            numReviews: 13,
+            countInStock: 6
+        },
+        {
+            _id: "2",
+            name: "Nike Men's Track Pants",
+            category: "Pants",
+            image: "/images/d2.jpg",
+            price: 50,
+            brand: " Nike",
+            rating: 4.2,
+            numReviews: 66,
+            countInStock: 8
+        },
+        {
+            _id: "3",
+            name: "Nike Men's Track Pants Gray",
+            category: "Pants",
+            image: "/images/d3.jpg",
+            price: 70,
+            brand: " Nike",
+            rating: 4,
+            numReviews: 8,
+            countInStock: 8
+        },
+        {
+            _id: "4",
+            name: "Nike Girl's Plain Regular fit T-Shirt",
+            category: "Pants",
+            image: "/images/d4.jpg",
+            price: 40,
+            brand: " Nike",
+            rating: 3.5,
+            numReviews: 18,
+            countInStock: 9
+        },
+        {
+            _id: "5",
+            name: "Spring Outfit",
+            image: "https://stock.adobe.com/images/white-dress-orange-shorts-with-kids-handbag-and-sunglasses-set-of-baby-clothes-and-accessories-for-spring-or-summer-on-beige-background-fashion-childs-outfit-flat-lay-top-view/318308547",
+            brand: "Fashion Kids",
+            price: 29.99,
+            category: "Children",
+            countInStock: 50,
+            description: "A cute spring outfit for kids including a white dress and orange shorts.",
+            rating: 4.5,
+            numReviews: 10
+        }
+    ]
+};
---
+++ frontend/src/data.js
@@ -0,0 +1,56 @@
+export default {
+    products: [
+        {
+            _id: "1",
+            name: "Nike Men's Regular fit T-Shirt",
+            category: "Shirt",
+            image: "/images/d1.jpg",
+            price: 35,
+            brand: " Nike",
+            rating: 3.0,
+            numReviews: 13
+        },
+        {
+            _id: "2",
+            name: "Nike Men's Track Pants",
+            category: "Pants",
+            image: "/images/d2.jpg",
+            price: 50,
+            brand: " Nike",
+            rating: 4.2,
+            numReviews: 66
+        },
+        {
+            _id: "3",
+            name: "Nike Men's Track Pants Gray",
+            category: "Pants",
+            image: "/images/d3.jpg",
+            price: 70,
+            brand: " Nike",
+            rating: 4,
+            numReviews: 8
+        },
+        {
+            _id: "4",
+            name: "Nike Girl's Plain Regular fit T-Shirt",
+            category: "Pants",
+            image: "/images/wf4.jpg",
+            price: 40,
+            brand: " Nike",
+            rating: 3.5,
+            numReviews: 18
+        },
+        {
+            _id: "5",
+            name: "Spring Outfit",
+            image: "/images/w1.jpg",
+            brand: "Fashion Kids",
+            price: 29.99,
+            category: "Children",
+            countInStock: 50,
+            description: "A cute spring outfit for kids including a white dress and orange shorts.",
+            rating: 4.5,
+            numReviews: 10
+        }
+    ]
+};
